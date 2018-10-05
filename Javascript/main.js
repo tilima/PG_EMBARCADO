55c372c9ced3d7ce850060d7f5a58e281fdedd79
Semprinil <lucas.semprini@hotmail.com>
2018-10-04 23:36:38 -0300

    04/10

--- Javascript/main.js
+++ Javascript/main.js
@@ -269,7 +269,8 @@ function newGraph(){
             }
         }
         });
-    } if(x=="C10"){
+    }
+    if(x=="C10"){
         var ctx = document.getElementById('myChart').getContext('2d');
         var chart = new Chart(ctx, {
         type: 'bar',
