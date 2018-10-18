99711d84d6ac266decbdebf784271e1d0f7ae85e
Semprinil <lucas.semprini@hotmail.com>
2018-10-18 19:36:01 -0300

    Código Escravo Pronto

--- PG_NANO_0x02/PG_NANO_0x02.ino
+++ PG_NANO_0x02/PG_NANO_0x02.ino
@@ -1,13 +1,12 @@
 #include <Wire.h>
 #include <EmonLib.h>
 
-//    DECLARA OS MEDIDORES   //
+//    MEDIDORES   //
 EnergyMonitor emon1;
 EnergyMonitor emon2;
 EnergyMonitor emon3;
 
-//   DECLARA GRANDEZAS   //
-
+//   FLAG   //
 int flag = 0;
 
 //    MEDIDOR 01    //
@@ -31,10 +30,7 @@ int flag = 0;
   float supplyVoltage3;              
   float Irms3;                        
 
-//   FLAG GLOBAL   //
-boolean flag_envio = false;
-
-//   DECLARA MENSAGEM   //
+//   TAMANHO MENSAGEM   //
 char size_1c[4];
 char size_2c[4];
 char size_3c[4];
@@ -50,7 +46,6 @@ char size_12c[4];
 char size_13c[4];
 char size_14c[4];
 char size_15c[4];
-
 String size_1;
 String size_2;
 String size_3;
@@ -67,30 +62,28 @@ String size_13;
 String size_14;
 String size_15;
 
-String dados_arduino;
-
-//   MEDIDOR 01   //
+//   MENSAGEM MEDIDOR 01   //
 char rP1[8];
 char aP1[8];
 char pF1[8];
 char sV1[8];
 char Ir1[8];
 
-//   MEDIDOR 02   //
+//   MENSAGEM MEDIDOR 02   //
 char rP2[8];
 char aP2[8];
 char pF2[8];
 char sV2[8];
 char Ir2[8];
 
-//   MEDIDOR 03   //
+//   MENSAGEM MEDIDOR 03   //
 char rP3[8];
 char aP3[8];
 char pF3[8];
 char sV3[8];
 char Ir3[8];
 
-//    DECLARA OS PINOS DO ARDUINO   //
+//    SENSORES   //
 const int SensorCorrente_1 = A0;
 const int SensorTensao_1 = A1;
 const int SensorCorrente_2 = A2;
@@ -101,22 +94,26 @@ const int SensorTensao_3 = A7;
 //    ORGANIZA OS DADOS MEDIDOS    //
 void prepara_dado(){
 
+  //   CONVERTE FLOAT PARA STRING   //
   dtostrf(realPower1, String(realPower1).length(), 2, rP1);
   dtostrf(apparentPower1, String(apparentPower1).length(), 2, aP1);
   dtostrf(powerFActor1, String(powerFActor1).length(), 2, pF1);
   dtostrf(supplyVoltage1, String(supplyVoltage1).length(), 2, sV1);
-  dtostrf(Irms1, String(Irms1).length(), 2, Ir1); 
+  dtostrf(Irms1, String(Irms1).length(), 2, Ir1);
+
   dtostrf(realPower2, String(realPower2).length(), 2, rP2);
   dtostrf(apparentPower2, String(apparentPower2).length(), 2, aP2);
   dtostrf(powerFActor2, String(powerFActor2).length(), 2, pF2);
   dtostrf(supplyVoltage2, String(supplyVoltage2).length(), 2, sV2);
   dtostrf(Irms2, String(Irms2).length(), 2, Ir2); 
+
   dtostrf(realPower3, String(realPower3).length(), 2, rP3);
   dtostrf(apparentPower3, String(apparentPower3).length(), 2, aP3);
   dtostrf(powerFActor3, String(powerFActor3).length(), 2, pF3);
   dtostrf(supplyVoltage3, String(supplyVoltage3).length(), 2, sV3);
   dtostrf(Irms3, String(Irms3).length(), 3, Ir3); 
 
+  //  ARMAZENA O TAMANHO DA STRING   //
   size_1 = String(realPower1).length();  
   size_2 = String(apparentPower1).length();
   size_3 = String(powerFActor1).length();
@@ -133,6 +130,7 @@ void prepara_dado(){
   size_14 = String(supplyVoltage3).length();
   size_15 = String(Irms3).length();
   
+  //   CONVERTE EM CHAR PARA ENVIO   //
   size_1.toCharArray(size_1c,String(realPower1).length());
   size_2.toCharArray(size_2c,String(apparentPower1).length());
   size_3.toCharArray(size_3c,String(powerFActor1).length());
@@ -150,9 +148,10 @@ void prepara_dado(){
   size_15.toCharArray(size_15c,String(Irms3).length());
 }
 
-//   ENVIA OS DADOS  //
+//   ENVIA OS DADOS   //
 void requestEvent() {
   switch (flag) {
+    
     case 0:
       Wire.write(size_1c);
       Wire.write(size_2c);
@@ -256,32 +255,6 @@ void loop() {
   Irms3            = emon3.Irms;             
 
   prepara_dado();
-//  Serial.println(dados_arduino);
-  Serial.println(size_1);
-  Serial.println(size_1c);
-  Serial.println(rP1);
-  Serial.println(aP1);
-  Serial.println(pF1);
-  Serial.println(sV1);
-  Serial.println(Ir1);
-  Serial.println(rP2);
-  Serial.println(aP2);
-  Serial.println(pF2);
-  Serial.println(sV2);
-  Serial.println(Ir2);
-  Serial.println(rP3);
-  Serial.println(aP3);
-  Serial.println(pF3);
-  Serial.println(sV3);
-  Serial.println(Ir3);
-  //Serial.println(realPower1);
-  //Serial.println(String(apparentPower1).length());
-  //Serial.println(aP1);
-  //Serial.println(apparentPower1);
-  //Serial.println(powerFActor1);
-  //Serial.println(supplyVoltage1);
-  //Serial.println(Irms1);
-  //dados_arduino = "";
+  
   delay(1);
 }
-
