ff1f9b22c9fe823254494b39a247400b557c92b6
Semprinil <lucas.semprini@hotmail.com>
2018-11-09 17:01:47 -0200

    XML e Graph OK

--- PG_V2/PG_V2.ino
+++ PG_V2/PG_V2.ino
@@ -659,7 +659,7 @@ boolean iniciar_sd_card() {
 
 // MAC address from Ethernet shield sticker under board
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 15, 115); // IP address, may need to change depending on network
+IPAddress ip(192, 168, 42, 115); // IP address, may need to change depending on network
 EthernetServer server(80);  // create a server at port 80
 File webFile;               // the web page file on the SD card
 char HTTP_req[REQ_BUF_SZ] = {0}; // buffered HTTP request stored as null terminated string
